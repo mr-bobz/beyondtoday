cb46659da42af1bec5d4cf7775159bc7755553ea
latha.maranganti@gmail.com <latha.maranganti@gmail.com@e951ad86-dd4c-a5b6-81ad-21afb94d0466>
2012-11-17 17:40:05 +0000

    spanish translation for PartnerCouncil.js & VolunteerCouncil.js

---
+++ src/dojo/bt/nls/es/PartnerCouncil.js
@@ -0,0 +1,7 @@
+define(
+	({	
+		"partner_title": "socio Consejo",
+		
+		"partner_para1" : "En curso"	
+	})
+);
---
+++ src/dojo/bt/nls/es/VolunteerCouncil.js
@@ -0,0 +1,9 @@
+define(
+	({	
+		"volunteer_title": "Voluntarios Consejo",
+		
+		"volunteer_para1" : "Los voluntarios son el alma de Beyond Today. El prop&#243;sito del Consejo de Voluntarios es:",
+
+		"volunteer_para2" :	"<li>Reclutar y entrenar a nuevos voluntarios al mismo tiempo mantener los voluntarios actuales comprometido y apasionado M&#225;s all&#225; del trabajo de hoy. </ Li> <li> Coordinar el individuo diferente y las actividades del grupo de voluntarios que tienen lugar en el &#225;mbito de las iniciativas m&#225;s all&#225; de hoy.</li> <li>Campe&#243;n M&#225;s all&#225; del trabajo de hoy en la comunidad.</li>"
+	})	
+);
