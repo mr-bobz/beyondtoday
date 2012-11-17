fedf3fb9ec8fb57a9afe074ec2514061eb02bce8
latha.maranganti@gmail.com <latha.maranganti@gmail.com@e951ad86-dd4c-a5b6-81ad-21afb94d0466>
2012-11-17 16:47:35 +0000

    adding spanish AdvisoryCouncil.js

---
+++ src/dojo/bt/nls/es/AdvisoryCouncil.js
@@ -0,0 +1,10 @@
+define(
+({	
+	"advisory_title": "Consejo Asesor",
+	
+	"advisory_para1" : "El Consejo Consultivo est&#225; integrado por l&#237;deres de la comunidad reflexivos que prestan su experiencia y visi&#243;n estrat&#233;gica a la junta de directores de la actualidad m&#225;s all&#225; y el personal. Representan una contribuci&#243;n program&#225;tica, hacer sugerencias y asesorar sobre las cuestiones relativas a las necesidades de organizaci&#243;n y liderazgo de la comunidad.",
+	
+	"advisory_para2" : "Debbie Trammell sirve de la Comunidad Asistente persona Relaciones y Coordinador de Voluntarios en Austin State Hospital. Debbie se asegura de que todos los procedimientos adecuados llevar&#225; a cabo dentro de este organismo del Estado desde una perspectiva de relaciones con la comunidad y es una de las personas m&#225;s positivas y muy respetado en el campus de ASH. Ella es responsable de muchos de los eventos de clase mundial que se producen en el hospital y proporciona conocimiento y aceptaci&#243;n de los problemas de salud mental en la comunidad. Debbie tambi&#233;n se gana la simpat&#237;a de los numerosos voluntarios que apoya ASH - de los estudiantes que quieren ser m&#233;dicos para los consumidores que quieren ser sus compa&#241;eros. <br/> Debbie tambi&#233;n es un 'ir a' una persona debe alguna vez necesitas ayuda. Despu&#233;s de d&#233;cadas de servicio en el hospital, ella est&#225; familiarizado con los entresijos y siempre se puede contar. Ella es un firme defensor de la salud mental en general y apoya los esfuerzos m&#225;s all&#225; de hoy en Apoyo de los compa&#241;eros, Recuperaci&#243;n, Educaci&#243;n y Pol&#237;tica informar."
+
+	})
+);
