d976cb2abf4054f727b6dbe41b73b3e07261993d
latha.maranganti@gmail.com <latha.maranganti@gmail.com@e951ad86-dd4c-a5b6-81ad-21afb94d0466>
2012-11-17 16:49:26 +0000

    localization changes for AdvisoryCouncil.js

--- src/dojo/bt/nls/AdvisoryCouncil.js
+++ src/dojo/bt/nls/AdvisoryCouncil.js
@@ -6,5 +6,6 @@ define({root:
 	
 	"advisory_para2" : "Debbie Trammell serves as the Assistant Community Relations person and Volunteer Coordinator at Austin State Hospital. Debbie ensures that all proper procedures take place within this State agency from a Community Relations perspective and is one of the most positive and well-respected persons on the ASH campus. She is responsible for many of the world-class events that occur at the hospital and provides awareness and acceptance of mental health issues within the community. Debbie also endears  herself to the numerous volunteers that ASH supports - from students wanting to be clinicians to consumers that want to be peers. <br/><br/>Debbie is also a 'go to' person should one ever need assistance. After decades of serving the hospital, she is familiar with the ins and outs and can always be counted upon. She is a strong supporter of mental health issues in general and supports Beyond Today's efforts in Peer Support, Recovery, Education and Informing Policy."
 
-	})
+	}),
+	"es": true
 });
